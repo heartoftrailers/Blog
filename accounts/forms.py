3878522355c1526d0e433667afa04616aa6b3ca4
Alex <iadenaida@yahoo.com>
2020-12-01 10:22:10 +0000

    User form added

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,10 @@ from django.contrib.auth.models import User
 class RegisterForm(UserCreationForm):
 
     email = forms.EmailField()
+    phone_number = forms.IntegerField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email','phone_number', 'password1', 'password2')
+
+        
--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 from django.urls import reverse_lazy
 from django.views import generic
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
